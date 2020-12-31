524feb31adb3115881cf2011d595be5686770857
SeoJun Park <jinchul112@gmail.com>
2020-12-31 11:32:29 +0100

    u

---
+++ src/api/User/SeeUser/SeeUser.resolvers.ts
@@ -0,0 +1,39 @@
+import { Resolvers } from "../../../types/resolvers";
+import { SeeUserQueryArgs, SeeUserResponse } from "../../../types/graph";
+import User from "../../../entities/User";
+
+const resolvers: Resolvers = {
+  Query: {
+    SeeUser: async (
+      _,
+      args: SeeUserQueryArgs,
+      { request, isAuthenticated }
+    ): Promise<SeeUserResponse> => {
+      const { userId } = args;
+      try {
+        const user = await User.findOne({ id: userId });
+        if (user) {
+          return {
+            ok: true,
+            err: null,
+            user
+          };
+        } else {
+          return {
+            ok: false,
+            err: "user not found",
+            user: null
+          };
+        }
+      } catch (err) {
+        return {
+          ok: false,
+          err: err.message,
+          user: null
+        };
+      }
+    }
+  }
+};
+
+export default resolvers;
