4fc9b3501290816dfbf625ee5bc218a45f5db35c
SeoJun Park <jinchul112@gmail.com>
2020-12-31 11:26:15 +0100

    u

---
+++ src/api/Post/GetPost/GetPost.resolvers.ts
@@ -0,0 +1,40 @@
+import { Resolvers } from "../../../types/resolvers";
+import { GetPostQueryArgs, GetPostResponse } from "../../../types/graph";
+import Post from "../../../entities/Post";
+
+const resolvers: Resolvers = {
+  Query: {
+    GetPost: async (
+      _,
+      args: GetPostQueryArgs,
+      { request, isAuthenticated }
+    ): Promise<GetPostResponse> => {
+      isAuthenticated(request);
+      const { postId } = args;
+      try {
+        const post = await Post.findOne({ id: postId });
+        if (post) {
+          return {
+            ok: true,
+            err: null,
+            post
+          };
+        } else {
+          return {
+            ok: false,
+            err: "No post found",
+            post: null
+          };
+        }
+      } catch (err) {
+        return {
+          ok: false,
+          err: err.message,
+          post: null
+        };
+      }
+    }
+  }
+};
+
+export default resolvers;
